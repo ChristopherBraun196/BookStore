de51f9f2f2b0334354203070166337c015bd4f67
Christopher Braun <Christopher-Braun@outlook.com>
2026-03-01 09:50:44 +0100

    add new db js

---
+++ db.new.js
@@ -0,0 +1,353 @@
+let newBooks = [
+  {
+    id: 0,
+    name: "Band 1: Der letze Wunsch",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 18.00,
+    publishedYear: 1993,
+    genre: "Fantasy",
+    isbn: "978-3-423-26264-4",
+    image: "./img/books/der-letzte-wunsch-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Band 2: Das Schwert der Vorsehung",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2009,
+    genre: "Fantasy",
+    isbn: "978-3-423-26265-1",
+    image: "./img/books/das-schwert-der-vorsehung-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Band 3: Das Erbe der Elfin",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2008,
+    genre: "Fantasy",
+    isbn: "978-3-423-26244-6",
+    image: "./img/books/das-erbe-der-elfen-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Band 4: Die Zeit der Verachtung",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2009,
+    genre: "Fantasy",
+    isbn: "978-3-423-26245-3",
+    image: "./img/books/die-zeit-der-verachtung-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Band 5: Feuertaufe",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2009,
+    genre: "Fantasy",
+    isbn: "978-3-423-26248-4",
+    image: "./img/books/feuertaufe-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Band 6: Das Schwalbenturm",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2010,
+    genre: "Fantasy",
+    isbn: "978-3-423-26246-0",
+    image: "./img/books/der-schwalbenturm-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Band 7: Die Dame vom See",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2011,
+    genre: "Fantasy",
+    isbn: "978-3-423-26247-7",
+    image: "./img/books/die-dame-vom-see-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Band 8: Etwas endet, etwas beginnt",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2012,
+    genre: "Fantasy",
+    isbn: "978-3-423-26274-3",
+    image: "./img/books/etwas-endet-etwas-beginnt-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Band 9: Zeit des Sturms",
+    author: "Andrzej Sapkowski",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2015,
+    genre: "Fantasy",
+    isbn: "978-3-423-26266-8",
+    image: "./img/books/zeit-des-sturms-taschenbuch-andrzej-sapkowski.jpeg",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+];
